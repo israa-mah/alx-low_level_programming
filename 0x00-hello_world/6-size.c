278c4eb6c924deac279fc3c4921d35d2d692186d
israa <esramahmoud38@gmial.com>
2023-08-10 19:31:10 -0700

    sizes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,6 +1,6 @@
-#include <stdio>
+#include <stdio.h>
 /**
- * main - A program print the vairable size
+ * main - variuos sizes
  * Return: 0 (success)
  */
 int main(void)
@@ -10,11 +10,11 @@ int main(void)
 	long int c;
 	long long int d;
 	float e;
-	printf("Size of a char:%lu bytes(s)\n", (unsigned long)sizeof(a));
-	printf("Size of a int:%lu bytes(s)\n", (unsigned long)sizeof(b));
-	printf("Size of a long int:%lu bytes(s)\n", (unsigned long)sizeof(c));
-	printf("Size of a long long int:%lu bytes(s)\n", (unsigned long)sizeof(d));
-	printf("Size of a float:%lu bytes(s)\n", (unsigned long)sizeof(e));
-		return (0);
 
+ printf("Size of a char:%lu bytes(s)\n", (unsigned long)sizeof(a));
+ printf("Size of a int:%lu bytes(s)\n", (unsigned long)sizeof(b));
+ printf("Size of a long int:%lu bytes(s)\n", (unsigned long)sizeof(c));
+ printf("Size of a long long int:%lu bytes(s)\n", (unsigned long)sizeof(d));
+ printf("Size of a float:%lu bytes(s)\n", (unsigned long)sizeof(e));
+ return (0);
 }
