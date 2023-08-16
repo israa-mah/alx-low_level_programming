98b39353e0b404619da9082945f950f4e1e6ff0c
israa <esramahmoud38@gmial.com>
2023-08-16 10:36:56 -0700

    lowercase x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print in lowercase 10 times
+ *
+ * Return: 0 always
+ */
+void print_alphabet_x10(void)
+{
+	int a = 0;
+	char alpha;
+
+	while (a >= 9)
+	{
+
+		for (alpha = 'a'; alpha <= 'z'; alpha++)
+			_putchar(alpha);
+		 _putchar('\n');
+			 a++;
+	}
+}
